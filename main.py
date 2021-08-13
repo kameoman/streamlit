5b04d63d76e44d5447d5b38cf681b3b8bab0e070
kameoman <spiderman.935963@gmail.com>
2021-08-13 09:42:31 +0900

    Interactive Widgets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import write
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -5,11 +6,58 @@ from PIL import Image
 
 st.title('Streamlit入門')
 st.write('DataFrame')
-st.write('Display Image')
+st.write('Interactive Widgets')
 
-img = Image.open('top.png')
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここが右カラムです')
 
-st.image(img, caption='agris top', use_column_width=True)
+option = st.text_input('あなたの趣味は？')
+condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+
+'あなたの趣味は', option,'です。'
+'コンディション:', condition
+
+expander = st.expander('お問い合わせ1')
+expander.write('お問い合わせ内容を書く')
+expander2 = st.expander('お問い合わせ2')
+expander2.write('お問い合わせ内容を書く')
+expander3 = st.expander('お問い合わせ3')
+expander3.write('お問い合わせ内容を書く')
+
+# bt = st.button('成功')
+# bt
+
+# rd = st.radio('ラジオボタン',('Comedy', 'Drama', 'Documentary'))
+# rd
+
+# option = st.selectbox(
+#   'あなたの好きな数字を入力してください',
+#   list(range(1, 11))
+# )
+
+# 'あなたの好きな数字は', option, 'です。'
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('top.png')
+#     st.image(img, caption='agris top', use_column_width=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # df = pd.DataFrame(
