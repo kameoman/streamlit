e3b596eee90915ad6541278f918254d25a05335c
kameoman <spiderman.935963@gmail.com>
2021-08-13 10:01:35 +0900

    streamlit-lesson-finish

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from os import write
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
+import time
 
 st.title('Streamlit入門')
 st.write('DataFrame')
-st.write('Interactive Widgets')
+st.write('プログレスバーの表示')
+'start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!'
+
 
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
