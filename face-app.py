bc28455bb70cb4b397cb86b63d118a8351014b03
kameoman <spiderman.935963@gmail.com>
2021-08-14 09:30:12 +0900

    add-results

--- face-app.py
+++ face-app.py
@@ -10,7 +10,7 @@ st.title('顔認識アプリ')
 with open('secret.face.json') as f:
     secret_json = json.load(f)
 
-subscription_key = [("SUBSCRIPTION_KEY")]
+subscription_key = secret_json["SUBSCRIPTION_KEY"]
 assert subscription_key
 
 face_api_url = 'https://20210813spiderkame.cognitiveservices.azure.com/face/v1.0/detect'
@@ -38,7 +38,18 @@ if uploaded_file is not None:
   for result in results:
       rect = result['faceRectangle']
 
+      at_rect = result['faceAttributes']
+      gender = at_rect['gender']
+      age = at_rect['age']
+      smile = at_rect['smile']
+
       draw = ImageDraw.Draw(img)
       draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'],rect['top']+rect['height'])], fill=None, outline='green', width=5)
+      draw.text([(45+rect['left'], 10+rect['top']+rect['height'])],gender)
+      draw.text([(rect['left'], 10+rect['top']+rect['height'])],'gender:')
+      draw.text([(30+rect['left'], 20+rect['top']+rect['height'])],str(age))
+      draw.text([(rect['left'], 20+rect['top']+rect['height'])],'age:')
+      draw.text([(40+rect['left'], 30+rect['top']+rect['height'])],str(smile))
+      draw.text([(rect['left'], 30+rect['top']+rect['height'])],'smile:')
   st.image(img, caption='Uploaded Image.', use_column_width=True)
 
